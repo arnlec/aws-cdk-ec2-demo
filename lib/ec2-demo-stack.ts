668ef845aeea7542c9596ed9b5334b6d3e09e0f9
Arnaud Le Cann <arnaud.le.cann@ericsson.com>
2023-02-22 15:59:45 +0000

    an environment with http proxy

--- lib/ec2-demo-stack.ts
+++ lib/ec2-demo-stack.ts
@@ -3,11 +3,14 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import { KeyPair } from 'cdk-ec2-key-pair';
 import { Construct } from 'constructs';
+import { SubnetType } from 'aws-cdk-lib/aws-ec2';
 
 
 const vpcId='vpc-1d4ad478';
 const availabilityZone = "eu-west-1a";
 const privateSubnetCidrBlock = "172.31.100.0/24";
+const squidPort=3128;
+const amiId='ami-08be94d0e177189de'; // Centos Stream 9 / eu-west-1
 
 export class Ec2DemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,6 +18,7 @@ export class Ec2DemoStack extends cdk.Stack {
 
     const vpc = ec2.Vpc.fromLookup(this,"VPC",{vpcId:vpcId});
 
+    // Key pair
     const key = new KeyPair(
       this,
       `${ id }key`,
@@ -24,6 +28,7 @@ export class Ec2DemoStack extends cdk.Stack {
     });
     new cdk.CfnOutput(this,'BastionKeyName',{ value: key.keyPairName });
  
+    // bastion instance
     const bastion = new ec2.BastionHostLinux(
       this,
       `${ id }Bastion`,
@@ -35,13 +40,54 @@ export class Ec2DemoStack extends cdk.Stack {
         }
       }
     );
-    bastion
     bastion.allowSshAccessFrom(ec2.Peer.anyIpv4());
     bastion.instance.instance.addPropertyOverride('KeyName',key.keyPairName);
     new cdk.CfnOutput(this,'BastionId',{ value: bastion.instanceId});
     new cdk.CfnOutput(this, 'BastionPublicDNS', { value: bastion.instancePublicDnsName });
     new cdk.CfnOutput(this, 'BastionPublicIP', { value:bastion.instancePublicIp });    
+ 
+    // proxy instance
+    const proxySecurityGroup = new ec2.SecurityGroup(
+      this,
+      `${ id }ProxySecurityGroup`,
+      {
+        vpc: vpc,
+        allowAllOutbound: true
+      }
+    )
 
+    proxySecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(`${bastion.instancePrivateIp}/32`),
+      ec2.Port.tcp(22),
+      'Allow SSH access from bastion'
+    );
+    proxySecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(privateSubnetCidrBlock),
+      ec2.Port.tcp(squidPort),
+      'Allow squid access from workers'
+    )
+
+    const proxy = new ec2.Instance(
+      this,
+      `${id}Proxy`,
+      {
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2,ec2.InstanceSize.NANO),
+        machineImage: new ec2.AmazonLinuxImage({
+          generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+        }),
+        vpc: vpc,
+        availabilityZone: availabilityZone,
+        keyName: key.keyPairName,
+        securityGroup: proxySecurityGroup,
+        vpcSubnets: {
+          subnetType: SubnetType.PUBLIC
+        }
+      }
+    )
+
+    new cdk.CfnOutput(this,'ProxyIP',{value: proxy.instancePrivateIp})  
+
+    // workers
     const privateSubnet = new ec2.PrivateSubnet(
       this,
       `${id}PrivateSubnet`,
@@ -65,15 +111,22 @@ export class Ec2DemoStack extends cdk.Stack {
       ec2.Port.tcp(22),
       'Allow SSH access from bastion'
     );
+    securityGroup.addEgressRule(
+      ec2.Peer.ipv4(`${proxy.instancePrivateIp}/32`),
+      ec2.Port.tcp(squidPort),
+      'Allow access to proxy'
+    );
 
     const instance1 = new ec2.Instance(
       this,
       `${id}Instance1`,
       {
         instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2,ec2.InstanceSize.NANO),
-        machineImage: new ec2.AmazonLinuxImage({
+        machineImage: ec2.MachineImage.genericLinux({
+          'eu-west-1': amiId
+        })/*new ec2.AmazonLinuxImage({
           generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
-        }),
+        })*/,
         vpc: vpc,
         availabilityZone: availabilityZone,
         keyName: key.keyPairName,
@@ -86,6 +139,7 @@ export class Ec2DemoStack extends cdk.Stack {
     new cdk.CfnOutput(this,'Instance1PrivateDNS',{value: instance1.instancePrivateDnsName})
     new cdk.CfnOutput(this,'Instance1IP',{value: instance1.instancePrivateIp})  
 
+    
     const endpoint = vpc.addGatewayEndpoint(`${id}GatewayEndpoint`,{
       service: ec2.GatewayVpcEndpointAwsService.S3,
       subnets: [{subnets:[privateSubnet]}]
@@ -101,6 +155,10 @@ export class Ec2DemoStack extends cdk.Stack {
         effect: iam.Effect.ALLOW
       })
     );
+    
+
+    
+
     
   }
 }
